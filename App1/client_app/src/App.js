7d15bc5fd5789715d174c1023b0dca5317ea62b9
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-11 17:51:52 +0100

    set up Router

--- App1/client_app/src/App.js
+++ App1/client_app/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-
-import { Route } from 'react-router'
-import Home from './components/Home'
+import React from "react"
+import NavBar from "./components/NavBar"
+import { Router, Route, Switch } from "react-router-dom"
+import Profile from "./components/Profile"
+import history from "./utils/history"
+import Home from "./components/Home"
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
-  return (
-    <div className="App">
-          <Route exact path='/' component={Home}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router history={history}>
+                <header>
+                    <NavBar />
+                </header>
+                <Switch>
+                    <Route path="/" exact />
+                    <PrivateRoute path="/profile" component={Profile} />
+                    <Route path="/home" component={Home} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
