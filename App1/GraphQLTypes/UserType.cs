ab16dd91730bcb3b3f92af8a8e7a741a3a5a353e
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-13 17:36:04 +0100

    created GraphQL type for user

---
+++ App1/GraphQLTypes/UserType.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+using IOprojekt.Models;
+using System;
+
+namespace IOprojekt.GraphQLTypes
+{
+    public class UserType : ObjectGraphType<User>
+    {
+        public UserType()
+        {
+            Field(_ => _.Id);
+            Field(_ => _.FirstName);
+            Field(_ => _.LastName);
+            Field(_ => _.Email);
+            Field(_ => _.Gender);
+            Field(_ => _.Locale);
+            Field(_ => _.CreatedAt).DefaultValue(DateTime.Now);
+        }
+    }
+}
