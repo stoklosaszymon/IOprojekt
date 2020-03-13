f2c8bf2be3eb9127445def043dc8a9036b59444d
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-13 18:02:10 +0100

    added GraphQLQueryDto that represent the post data payload entity

---
+++ App1/GraphQLTypes/GraphQLQueryDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLSample.Models
+{
+    public class GraphQLQueryDto
+    {
+        public string Query { get; set; }
+    }
+}
