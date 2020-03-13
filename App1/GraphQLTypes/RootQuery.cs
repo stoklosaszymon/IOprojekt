ce0c7bb84451ab84027953edd468ef7fd26cf6ca
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-13 22:08:05 +0100

    added: auery arguments To filter data using id

--- App1/GraphQLTypes/RootQuery.cs
+++ App1/GraphQLTypes/RootQuery.cs
@@ -1,5 +1,6 @@
 ﻿using GraphQL.Types;
 using IOprojekt.Repositories;
+using System.Linq;
 
 namespace IOprojekt.GraphQLTypes
 {
@@ -10,6 +11,16 @@ namespace IOprojekt.GraphQLTypes
             Field<ListGraphType<UserType>>("users", resolve: context =>
             {
                 return _userRepository.GetAll();
+            });
+
+            Field<ListGraphType<UserType>>("users", arguments: new QueryArguments
+            {
+               new  QueryArgument<StringGraphType> { Name = "id"}
+            },
+            resolve: context =>
+            {
+                int id = context.GetArgument<int>("id");
+                return _userRepository.GetAll().Where(_ => _.Id == id).ToList();
             });
         }
     }
