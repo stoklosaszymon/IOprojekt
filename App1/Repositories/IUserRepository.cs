8b06f090a2f820bd0ff291b27cb8247d7e194c60
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-13 15:53:15 +0100

    created user repository interface

---
+++ App1/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IOprojekt.Models;
+
+namespace IOprojekt.Repositories
+{
+        public interface IUserRepository
+        {
+            List<User> GetAll();
+        }
+}
