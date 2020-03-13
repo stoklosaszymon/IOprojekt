5893d7866c220492ccedcc192112deb86c099b2b
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-13 18:07:50 +0100

    added GraphQLController

---
+++ App1/Controllers/GraphQLController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using IOprojekt.Models;
+
+namespace IOprojekt.Controllers
+{
+    [Route("graphql")]
+    public class GraphQLController : Controller
+    {
+        private readonly ISchema _schema;
+        private readonly IDocumentExecuter _executer;
+        public GraphQLController(ISchema schema, IDocumentExecuter executer)
+        {
+            _schema = schema;
+            _executer = executer;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GraphQLQueryDto query)
+        {
+            var result = await _executer.ExecuteAsync(_ =>
+            {
+                _.Schema = _schema;
+                _.Query = query.Query;
+            }).ConfigureAwait(false);
+
+            if (result.Errors?.Count > 0)
+            {
+                return Problem(detail: result.Errors.Select(_ => _.Message).FirstOrDefault(), statusCode: 500);
+            }
+            return Ok(result.Data);
+        }
+    }
+}
--- App1/Models/GraphQLQueryDto.cs
+++ App1/Models/GraphQLQueryDto.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GraphQLSample.Models
+namespace IOprojekt.Models
 {
     public class GraphQLQueryDto
     {
