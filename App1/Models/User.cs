95606bb7935d2bb0b17269a1de9dbfa3bb71c189
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-13 15:50:10 +0100

    created user model

---
+++ App1/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOprojekt.Models
+{
+    public class User
+    {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Gender { get; set; }
+            public string Locale { get; set; }
+
+            [DataType(DataType.Date)]
+            public DateTime CreatedAt { get; set; }
+    }
+}
