2e327c251b537cb17a5bb18c39a42fd36c39e25f
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-18 21:31:29 +0100

    added IRepository

---
+++ App1/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOprojekt.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        string CollectionName { get; }
+        Task<long> CountAsync(FilterDefinition<TEntity> filter);
+        IFindFluent<TEntity, TEntity> Find(FilterDefinition<TEntity> filter);
+        IFindFluent<TEntity, TEntity> Find(Expression<Func<TEntity, bool>> filter);
+        Task<TEntity> FindOneAndReplaceAsync(FilterDefinition<TEntity> filter, TEntity replacement);
+        Task<TEntity> FindOneAndReplaceAsync(Expression<Func<TEntity, bool>> filter, TEntity replacement);
+    }
+}
+}
