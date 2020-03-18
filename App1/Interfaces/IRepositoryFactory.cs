d7a711a138092814e75ee0beb04be95f2cd21e4f
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-18 21:37:31 +0100

    added IRepositoryFactory

---
+++ App1/Interfaces/IRepositoryFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOprojekt.Interfaces
+{
+    public interface IRepositoryFactory
+    {
+        IRepository<TEntity> Create<TEntity>(RepositoryOptions options);
+    }
+}
