e63d80034ad2b8e91f47d603a912ff3f6018135b
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-18 15:09:36 +0100

    created user repository tests, installed Mock package

---
+++ Bcknd_Tests/UserRepositoryTest.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+namespace Bcknd_Tests
+{
+    public class UserRepositoryTest
+    {
+        private Mock<IOptions<Mongosettings>> _mockOptions;
+
+        private Mock<IMongoDatabase> _mockDB;
+
+        private Mock<IMongoClient> _mockClient;
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
