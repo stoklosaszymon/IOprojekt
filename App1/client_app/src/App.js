4a29bcf2d6f696d33a2ee59102c9be8ecd6bb75c
Ricoss <urbaniecmateusz2@gmail.com>
2020-03-18 13:45:34 +0100

    Modifying App.test.js And crete Chat.js

--- App1/client_app/src/App.js
+++ App1/client_app/src/App.js
@@ -5,6 +5,7 @@ import Profile from "./components/Profile"
 import history from "./utils/history"
 import Home from "./components/Home"
 import PrivateRoute from './components/PrivateRoute'
+import Chat from './components/Chat'
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                     <Route path="/" exact />
                     <PrivateRoute path="/profile" component={Profile} />
                     <Route path="/home" component={Home} />
+                    <Route path="/Chat" component={Chat} />
                 </Switch>
             </Router>
         </div>
---
+++ App1/client_app/src/components/Chat.js
@@ -0,0 +1,46 @@
+﻿import React, { Component } from 'react';
+
+export default class Chat extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Chat page</h1>
+                <div className="container">
+                    <div className="row">&nbsp;</div>
+                    <div class="row">
+                        <div class="col-2">User</div>
+                        <div class="col-4"><input type="text" id="userInput" /></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-2">Message</div>
+                        <div class="col-4"><input type="text" id="messageInput" /></div>
+                    </div>
+                    <div class="row">&nbsp;</div>
+                    <div class="row">
+                        <div class="col-6">
+                            <input type="button" id="sendButton" value="Send Message" />
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <hr />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <ul id="messagesList"></ul>
+                    </div>
+                </div>
+                
+            </div>
+
+            
+        );
+    }
+}
