327e11d86d6b3e1e40dc66d294dd75698d2c920e
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-18 21:36:10 +0100

    created dbContext

---
+++ App1/Contexts/DbContext.cs
@@ -0,0 +1,25 @@
+﻿using IOprojekt.Interfaces;
+using IOprojekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOprojekt.Contexts
+{
+    public class DbContext : IDbContext
+    {
+        public DbContext(IRepositoryFactory repoFactory, string connectionString, string dbName)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString)) 
+                throw new ArgumentNullException("connectionString"); 
+            if (string.IsNullOrWhiteSpace(dbName)) 
+                throw new ArgumentNullException("dbName");
+            
+            this.Users = repoFactory.Create<User>(new RepositoryOptions(connectionString, dbName, "users")); 
+            
+        }
+        public IRepository<User> Users { get; private set; }
+
+    }
+}
