887e6c950d967df7ba59f4229cdf6ee840b182c5
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-18 21:39:35 +0100

    added RepositoryFactory

---
+++ App1/Repositories/RepositoryFactory.cs
@@ -0,0 +1,24 @@
+﻿using IOprojekt.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOprojekt.Repositories
+{
+    public class RepositoryFactory : IRepositoryFactory
+    {
+        private readonly IMongoDatabaseFactory _dbFactory;
+        public RepositoryFactory(IMongoDatabaseFactory dbFactory)
+        {
+            if (dbFactory == null) throw new ArgumentNullException("dbFactory");
+            _dbFactory = dbFactory;
+        }
+        public IRepository<TEntity> Create<TEntity>(RepositoryOptions options)
+        {
+            if (options == null) throw new ArgumentNullException("options");
+            var db = _dbFactory.Connect(options.ConnectionString, options.DbName);
+            return new Repository<TEntity>(db.GetCollection<TEntity>(options.CollectionName));
+        }
+    }
+}
