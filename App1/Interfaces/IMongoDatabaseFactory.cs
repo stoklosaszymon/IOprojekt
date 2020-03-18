b9ed307bc08094adec34f0eedd2d664de2dddd98
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-18 21:40:45 +0100

    added IMongoDatabaseFactory

---
+++ App1/Interfaces/IMongoDatabaseFactory.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOprojekt.Interfaces
+{
+    public interface IMongoDatabaseFactory
+    {
+        IMongoDatabase Connect(string connectionString, string dbName);
+    }
+}
