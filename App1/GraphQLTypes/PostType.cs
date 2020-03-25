8e93a840718a9b2e00b8be7e264e87de0c8cdb1d
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-25 19:12:36 +0100

    create PostType

---
+++ App1/GraphQLTypes/PostType.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL.Types;
+using IOprojekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOprojekt.GraphQLTypes
+{
+    public class PostType : ObjectGraphType<Post>
+    {
+        public PostType()
+        {
+            Field(_ => _.PostId);
+            Field(_ => _.UserId);
+            Field(_ => _.Body);
+            Field(_ => _.CreatedAt).DefaultValue(DateTime.Now);
+        }
+        
+    }
+}
