e169f957f72ec653320d9e8073318348e2d3165d
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-25 19:07:08 +0100

    create Post model

---
+++ App1/Models/Post.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IOprojekt.Models
+{
+    public class Post
+    {
+        [BsonId]
+        [BsonElement("postId")]
+        [BsonRepresentation(BsonType.Int64)]
+        public int PostId { get; set; }
+
+        [BsonId]
+        [BsonElement("postId")]
+        [BsonRepresentation(BsonType.Int64)]
+        public int UserId { get; set; }
+
+        [BsonElement("body")]
+        [BsonRepresentation(BsonType.String)]
+        public string Body { get; set; }
+
+
+        [BsonElement("createdAt")]
+        [BsonRepresentation(BsonType.DateTime)]
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+    }
+}
