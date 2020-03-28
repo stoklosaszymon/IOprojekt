5f94658e79230a97152f1539a0b00af1578c36ec
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-03-28 20:09:35 +0100

    import reducer to store

--- App1/client_app/src/components/NavBar.js
+++ App1/client_app/src/components/NavBar.js
@@ -1,19 +1,31 @@
 ﻿import React from 'react'
 import { useAuth0 } from "../react-auth0-spa";
+import { connect } from 'react-redux';
 
-const NavBar = () => {
+let NavBar = () => {
 
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
     return (
-        <>
+        <div>
             {!isAuthenticated && (
                     <button onClick={() => loginWithRedirect({})}>Log in</button>
             )}
 
             {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-        </>
+        </div>
   );
 };
 
-export default NavBar;
+const mapStateToProps = (state) => {
+    return { logged: state.logged, loggedUserId: state.loggedUserId };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLogIn: (userId) => dispatch({ type: 'LOG_IN', loggedUserId: userId }),
+        onLogOut: () => dispatch({ type: 'LOG_OUT' }),
+    }
+};
+
+export default NavBar = connect(mapStateToProps, mapDispatchToProps)(NavBar);
