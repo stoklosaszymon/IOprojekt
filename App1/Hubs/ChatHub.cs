bbf6bfe46b46baea5bb206e20ce32998d758decc
Ricoss <urbaniecmateusz2@gmail.com>
2020-04-06 17:17:44 +0200

    modification of SendMessageToAll

--- App1/Hubs/ChatHub.cs
+++ App1/Hubs/ChatHub.cs
@@ -5,9 +5,9 @@ namespace IOprojekt.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage (string user, string message)
+        public async Task SendMessageToAll(string user, string message)
         {
-            await Clients.All.SendAsync("SendMessage", user, message);
+            await Clients.All.SendAsync("SendMessageToAll", user, message);
         }
 
         public async Task JoinRoom (string name, string roomName)
