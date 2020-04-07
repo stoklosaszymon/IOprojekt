2aeb96f0f8d480f44e730da371ed3302673ee1fb
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-04-08 00:14:02 +0200

    add user to db on first time login

--- App1/client_app/src/components/NavBar.js
+++ App1/client_app/src/components/NavBar.js
@@ -3,9 +3,7 @@ import { useAuth0 } from "../react-auth0-spa";
 
 const NavBar = () => {
 
-    const { isAuthenticated, loginWithRedirect, logout, getTokenSilently } = useAuth0();
-
-
+    const { isAuthenticated, loginWithRedirect, logout, getTokenSilently, user } = useAuth0();
 
     return (
         <div>
@@ -15,10 +13,33 @@ const NavBar = () => {
                 )}
 
                 {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
-                    {isAuthenticated ? console.log(getTokenSilently()) : console.log("brak tokena")}
+                {isAuthenticated && user !== undefined ? addUser(user) : console.log(user)}
             </div>
         </div>
   );
 };
 
+const addUser = ({ sub, given_name, family_name, locale, email }) => {
+    fetch('graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            query: `
+                    mutation {
+                      users {
+                        addUser(user: {
+                                  sub: "${sub}",
+                                  firstName: "${given_name}",
+                                  lastName: "${family_name}",                         
+                                  locale: "${locale}",
+                                  email: "${email}",
+                                }) 
+                        {
+                           sub
+                        }
+                      }
+                    }`
+        }),
+    })
+}
 export default NavBar;
