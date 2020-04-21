4adcec3d764d5685f0aa6e7db4871aca61327038
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-04-21 22:00:39 +0200

    added friends model

---
+++ App1/Models/Friends.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IOprojekt.Models
+{
+    public class Friends
+    {
+        [BsonElement("userId")]
+        public int UserId { get; set; }
+
+        [BsonElement("friendsList")]
+        public BsonArray FriendsList { get; set; }
+    }
+}
