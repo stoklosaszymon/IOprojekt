8887a391cdb1586038760bb3de0404b2b0f4989c
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-04-24 21:47:37 +0200

    added friends query

---
+++ App1/GraphQLTypes/FriendsQuery.cs
@@ -0,0 +1,30 @@
+﻿using GraphQL.Types;
+using IOprojekt.Interfaces;
+using IOprojekt.Models;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace IOprojekt.GraphQLTypes
+{
+    public class FriendsQuery : ObjectGraphType
+    {
+        private readonly IDbContext _context;
+        public FriendsQuery(IDbContext context)
+        {
+            if (context != null)
+                _context = context;
+
+            Field<ListGraphType<UserType>>("GetFriendsByUserId",
+            arguments: new QueryArguments
+            {
+               new  QueryArgument<IntGraphType> { Name = "userId"}
+            },
+            resolve: context =>
+            {
+                var id = context.GetArgument<int>("userId");
+                var filter = Builders<Friends>.Filter.Eq(user => user.UserId, id);
+                return _context.Friends.GetAll(filter).Result;
+            });
+        }
+    }
+}
