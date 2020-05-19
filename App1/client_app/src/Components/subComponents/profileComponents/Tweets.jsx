f20d66daa9f69ca7233bd3550d174383ffb546c2
maniok112 <summerballowski@gmail.com>
2020-05-19 21:57:27 +0200

    Update Tweets.jsx

--- App1/client_app/src/Components/subComponents/profileComponents/Tweets.jsx
+++ App1/client_app/src/Components/subComponents/profileComponents/Tweets.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import Avatar from "../../assets/img/profile_normal.png";
 
 const Tweets = () => {
