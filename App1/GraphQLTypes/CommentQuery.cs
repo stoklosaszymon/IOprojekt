ef6a9b95d6dc024945a05ba323a3803ded633fb7
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-05-28 19:25:28 +0200

    add CommentQuery

---
+++ App1/GraphQLTypes/CommentQuery.cs
@@ -0,0 +1,31 @@
+﻿using GraphQL.Types;
+using IOprojekt.Interfaces;
+using IOprojekt.Models;
+using MongoDB.Driver;
+using System.Linq;
+
+namespace IOprojekt.GraphQLTypes
+{
+    public class CommentQuery : ObjectGraphType
+    {
+        private readonly IDbContext _context;
+        public CommentQuery(IDbContext context)
+        {
+            if (context != null)
+                _context = context;
+            Name = "CommentQuery";
+
+            Field<ListGraphType<CommentType>>("GetByPostId",
+            arguments: new QueryArguments
+            {
+               new  QueryArgument<StringGraphType> { Name = "postId"}
+            },
+            resolve: context =>
+            {
+                var postId = context.GetArgument<string>("postId");
+                var filter = Builders<Comment>.Filter.Eq(comm => comm.PostId, postId);
+                return _context.Comments.GetAll(filter).Result;
+            });
+        }
+    }
+}
