5eedea0cdf1402e9d70a414fffaa61934d917aa7
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-05-28 19:01:24 +0200

    add Comment ot dbcontext

--- App1/Contexts/DbContext.cs
+++ App1/Contexts/DbContext.cs
@@ -17,11 +17,13 @@ namespace IOprojekt.Contexts
             this.Users = repoFactory.Create<User>(new RepositoryOptions(connectionString, dbName, "Users"));
             this.Posts = repoFactory.Create<Post>(new RepositoryOptions(connectionString, dbName, "Posts"));
             this.Friends = repoFactory.Create<Friends>(new RepositoryOptions(connectionString, dbName, "Friends"));
+            this.Comments = repoFactory.Create<Comment>(new RepositoryOptions(connectionString, dbName, "Comments"));
 
         }
         public IRepository<User> Users { get; private set; }
         public IRepository<Post> Posts { get; private set; }
         public IRepository<Friends> Friends { get; private set; }
+        public IRepository<Comment> Comments { get; private set; }
 
     }
 }
