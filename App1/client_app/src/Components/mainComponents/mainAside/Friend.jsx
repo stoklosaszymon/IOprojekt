b7057483d8e013a818de3d49a7f8b55b8e70cd48
Ricoss <urbaniecmateusz2@gmail.com>
2020-06-09 17:35:14 +0200

    Adding friends list

---
+++ App1/client_app/src/Components/mainComponents/mainAside/Friend.jsx
@@ -0,0 +1,18 @@
+
+import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import FullName from "./../PostComponents/FullName";
+import MainAvatar from "./../PostComponents/MainAvatar";
+
+
+const Friend = ({ id }) => {
+
+
+    return (
+        <div >
+            <h1>ll</h1>
+        </div>
+    )
+};
+
+export default Friend;
