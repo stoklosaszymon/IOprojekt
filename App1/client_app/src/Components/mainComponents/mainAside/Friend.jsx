ba02c7fc60d079c995a8d568a1dfb2310a1f88ca
Ricoss <urbaniecmateusz2@gmail.com>
2020-06-09 17:38:51 +0200

    Method GET

--- App1/client_app/src/Components/mainComponents/mainAside/Friend.jsx
+++ App1/client_app/src/Components/mainComponents/mainAside/Friend.jsx
@@ -7,7 +7,47 @@ import MainAvatar from "./../PostComponents/MainAvatar";
 
 const Friend = ({ id }) => {
 
+    const [users, setUsers] = useState([]);
+    const [friends, setFriend] = useState([]);
 
+
+    useEffect(() => {
+
+        fetch('../graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query:
+                    `{
+                            friends {
+                               getFriendsByUserId
+                                         (userId: "${id}"){ userId friendsList}
+                                    }
+                       
+                    users {
+                               getAll {
+                                id firstName lastName picture nickname 
+                                      }
+                            }
+                    }`
+            }),
+        })
+            .then(res => res.json())
+            .then(res => {
+                setFriend(res.friends.getFriendsByUserId.friendsList);
+                setUsers(res.users.getAll);
+            })
+            .catch(err => console.error(err));
+
+    }, [id]);
+
+
+    let listFriend = friends.map(x => {
+        return ({ ...users.find(p => p.id === x, ...x) })
+    });
+    console.log(listFriend);
     return (
         <div >
             <h1>ll</h1>
