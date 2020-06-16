38236be02f9de389e70f149c0a792c743e4dd032
Ricoss <urbaniecmateusz2@gmail.com>
2020-06-16 20:18:41 +0200

    Login Chat Tests

---
+++ Bcknd_Tests/ChatHubLoginTests.cs
@@ -0,0 +1,36 @@
+﻿using IOprojekt.Hubs;
+using IOprojekt.Models;
+using IOprojekt.Utils;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using Xunit;
+
+namespace Bcknd_Tests
+{
+    public class ChatHubLoginTests :Hub
+    {
+        [Fact]
+        public void Login_iscalled_returnchatClients()
+        {
+            //Arrange
+            var chatHub = new ChatHub();
+            string firstName = "a";
+            Mock<HubCallerContext> mockClientContext = new Mock<HubCallerContext>(); 
+            mockClientContext.Setup(c => c.ConnectionId).Returns(Guid.NewGuid().ToString);
+
+             //Act
+             var  chatClients = chatHub.Login(firstName);
+
+            //Assert
+            Assert.Equal(firstName, chatClients.Select(x => x.Value.FirstName).First()); 
+            Assert.Equal(firstName, chatClients.Select(x => x.Key).First());
+            Assert.IsType<ConcurrentDictionary<string, User>>(chatClients);
+            
+
+        }
+
+    }
+}
