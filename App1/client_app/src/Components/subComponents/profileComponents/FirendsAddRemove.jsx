e083826d14e10a86255102c5958b64ac5340cfd4
Ricoss <urbaniecmateusz2@gmail.com>
2020-08-10 15:36:51 +0200

    Place to add friends and remove

---
+++ App1/client_app/src/Components/subComponents/profileComponents/FirendsAddRemove.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const FirendsAddRemove = ({}) => {
+
+    return ( 
+        <div className="stream-container">
+            <p>123</p>
+        </div> 
+  );
+};
+
+export default FirendsAddRemove;
