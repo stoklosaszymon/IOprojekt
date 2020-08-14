845a5429a613cd94b39724dd4c96bab2e435fa02
Ricoss <urbaniecmateusz2@gmail.com>
2020-08-14 18:31:15 +0200

    Toast Component

---
+++ App1/client_app/src/Components/subComponents/editCpmponents/EditBackground.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const EditBackground = ({ firstName }) => {
+
+    const [inputValue, setInputValue] = useState("");
+   
+    //const input = document.querySelector('input');
+    //const log = document.getElementById('log');
+
+    //input.addEventListener('change', updateValue);
+
+    //function updateValue(e) {
+    //    log.textContent = e.target.value;
+    //}
+
+    return (
+        <div className="updateFirstName-Container">
+            <strong className="FirstName">{firstName}</strong>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(
+                    ev: React.ChangeEvent<HTMLInputElement>,
+                ): void => {setInputValue(ev.target.value)}}
+            />
+            <p>{inputValue}</p>
+        </div>
+    );
+};
+export default EditBackground;
+
+
--- App1/client_app/src/Components/subComponents/profileComponents/ProfileSetting.jsx
+++ App1/client_app/src/Components/subComponents/profileComponents/ProfileSetting.jsx
@@ -1,12 +1,34 @@
-import React from "react";
-import ProfileUI from "./ProfileUI";
-import SearchLogo from "../../assets/SearchLogo";
-
+import React, { useState } from 'react';
+import PersonalData from "../../subComponents/editCpmponents/PersonalData"
+import EditBackground from "../../subComponents/editCpmponents/EditBackground"
+import EditAvatar from "../../subComponents/editCpmponents/EditAvatar"
 const ProfileSetting = () => {
 
+    const [Check, setCheck] = useState("");
+
     return (
-        <div>
-            <div> 123 </div>
+        <div className="edit-profile-container">
+            <section className="section">
+                <div className="display-list">
+                    <div className="point" key="avatar" onClick={(e) => setCheck('avatar')} > Edit profile picture</div>
+                    <div className="point" key="background" onClick={(e) => setCheck('background')}> Edit background picture</div>
+                    <div className="point" key="data" onClick={(e) => setCheck('data')}> edit personal details</div>
+                </div>
+            </section>
+            <aside className="asaid">
+                {(() => {
+                    switch (Check) {
+                    case "avatar":
+                        return <EditAvatar firstName="avatar" />;
+                    case "background":
+                        return <EditBackground firstName="background" />;
+                    case "data":
+                        return <PersonalData firstName="data" />;
+                    default:
+                        return <h1>What your soul desires </h1>;
+                    }
+                })()}
+            </aside>
         </div>
     );
 };
