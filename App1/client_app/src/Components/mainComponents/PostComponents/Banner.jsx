7aa739289610412d04f4da1f450f8793379eadfe
Ricoss <urbaniecmateusz2@gmail.com>
2020-08-16 15:11:32 +0200

    Ui Edit Banner

--- App1/client_app/src/Components/mainComponents/PostComponents/Banner.jsx
+++ App1/client_app/src/Components/mainComponents/PostComponents/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = ({ picture }) =>
     <div className="main-Banner">
-        <img src={picture} alt="avatar" className="main-Banner-img" />
+        <img src={picture} alt="banner" className="main-Banner-img"/>
     </div>
 
 export default Banner;
