ee3027606f5e0445a43a30a5601675123e655c06
Ricoss <urbaniecmateusz2@gmail.com>
2020-08-20 15:53:48 +0200

    Account

--- App1/client_app/src/Components/subComponents/moreComponents/Account.jsx
+++ App1/client_app/src/Components/subComponents/moreComponents/Account.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 
 const Account = () => {
+    const user = useSelector(state => state.loggedUser);
     
     return (
-        <div className="more-container">
-            <p>Account</p>
-        </div>
+    <div>
+        {(user.nickname === '')? 
+            <div>
+                <strong> when you log in you will see your details </strong>
+            </div>
+            :
+                <div className="Account-container">
+                    <strong className="writeFirstName"> First Name: </strong>
+                    <strong className="firstName"> {user.firstName} </strong>
+                    <br />
+                    <strong className="writeLastName"> Lasr Name: </strong>
+                    <strong className="lastName"> {user.lastName} </strong>
+                    <br />
+                    <strong className="writeEmail"> Email: </strong>
+                    <strong className="email"> {user.email} </strong>
+                    <br />
+                    <strong className="writeNickName"> Nickname: </strong>
+                    <strong className="nickName"> {user.nickname} </strong>
+                    <br />
+            </div >
+        }
+    </div>
     );
 };
 export default Account;
