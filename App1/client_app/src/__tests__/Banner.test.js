d8b3a9b23a4e8458d4b4c05097f46890a4ad7e28
Ricoss <urbaniecmateusz2@gmail.com>
2020-08-25 12:45:08 +0200

    Test Banner

---
+++ App1/client_app/src/__tests__/Banner.test.js
@@ -0,0 +1,32 @@
+﻿import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { configure, shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Banner from '../Components/mainComponents/PostComponents/Banner'
+
+
+configure({ adapter: new Adapter() });
+
+jest.mock('../react-auth0-spa');
+
+let bannerSrc = 'imgSource';
+
+describe('components/NavBar - logged in', () => {
+    it('Renders with comonent', () => {
+        const wrapper = mount(<Banner />);
+        expect(wrapper).toBeTruthy();
+    });
+    it('Renders banner ', () => {
+        const wrapper = mount(<Banner picture={bannerSrc}/>);
+        expect(wrapper).toBeTruthy();
+        expect(wrapper.find('img')).toHaveLength(1);
+    });
+    it('Renders banner with props', () => {
+        const wrapper = mount(<Banner picture={bannerSrc} />);
+        const imgWraper = wrapper.find('img');
+        expect(imgWraper.prop('src')).toEqual(bannerSrc);
+        expect(imgWraper.prop('alt')).toEqual('banner');
+        expect(imgWraper.prop('className')).toEqual("main-Banner-img");
+    });
+});
+
