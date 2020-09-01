fc578a663fc0268d30faa7ce3053b632184ffd29
stoklosa.szymon <stoklosa.szymon@gmail.com>
2020-09-01 16:26:45 +0200

    search bar

--- App1/client_app/src/Components/mainComponents/mainAside/Search.jsx
+++ App1/client_app/src/Components/mainComponents/mainAside/Search.jsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Search = () => {
-  return (
-    <div className="search-container">
+
+    const [users, setUsers] = useState([]);
+    const [filtered, setFiltered] = useState([]);
+
+     useEffect(() => {
+        fetch('graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `{ 
+                          users {
+                               getAll {
+                                id firstName lastName picture nickname 
+                            }
+                          }
+                         }`
+            })
+        })
+            .then(res => res.json())
+            .then(res => {
+                setUsers(res.users.getAll);
+            })
+     }, []);
+
+    let filterData = (event) => {
+        if ( event.target.value.length > 0)
+            setFiltered(users.filter(u => new RegExp(`${event.target.value}`).test(u.firstName)))
+        else
+            setFiltered([])
+
+        event.preventDefault();
+    }
+
+    return (
+        <div className="search-container">
       <div className="search">
-        <input type="text" name="search" placeholder="Search Twitter" />
+                <input type="text" name="search" placeholder="Search Twitter" onChange={filterData}/>
+                {
+                    filtered.map(user =>
+                        <div key={user.id}>
+                         <NavLink to={`/${user.nickname}`}>{`${user.firstName} ${user.lastName}`} </NavLink> 
+                       </div>
+                )}
       </div>
     </div>
   );
